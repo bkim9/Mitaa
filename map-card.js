bdee7e8fdbdfbf3d2704ca6d32d0dd3248ce4602
Beomsu Kim <bek001@ucsd.edu>
2023-12-06 15:00:46 -0800

    added map support

---
+++ map-card.js
@@ -0,0 +1,30 @@
+function init_map(idloc) {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYmtpbTkiLCJhIjoiY2xwdTl4bXR0MDNjNDJscXJ5Z2xxNXh1aCJ9.gsUYWzPvyhbFzLf2-IEHGQ';
+    const map = new mapboxgl.Map({
+        container: idloc.id, 
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: idloc.loc,
+        zoom: 16.5 
+    });
+    const marker = new mapboxgl.Marker()
+        .setLngLat(idloc.loc)
+        .addTo(map);
+}
+
+function init_maps(){    
+   idLocs= [
+    {
+        id: 'jkcmap',
+        loc: [-118.30722410802338, 34.06053851590417]
+    },
+    {
+        id: 'drkomap',
+        loc: [-118.44470618475876, 34.058902701314295]
+    }
+   ]
+   for (const idLoc of idLocs) {
+    init_map(idLoc);
+   }
+}
+
+window.addEventListener("DOMContentLoaded", init_maps);
---
+++ submit.js
@@ -0,0 +1,9 @@
+function sendEmail(name, email, message) {
+    email.send({
+        Host: "smtp.gmail.com",
+        Username: 'kbbss96@gmail.com',
+        Password: 'acewrgjwsybbeiyt',
+        To: "kbbss96@gmail.com",
+        
+    })
+}
