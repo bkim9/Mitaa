d259a70256eb1e7541ca71068c8fe1deff3ad7f3
Beomsu Kim <bek001@ucsd.edu>
2023-12-08 17:04:20 -0800

    ip

--- submit.js
+++ submit.js
@@ -1,3 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBAA028ZEhA_ylFS1zut-h9mvp5V6aoFw4",
+  authDomain: "mitaa-401922.firebaseapp.com",
+  projectId: "mitaa-401922",
+  storageBucket: "mitaa-401922.appspot.com",
+  messagingSenderId: "736885150902",
+  appId: "1:736885150902:web:192b10aa8e26abd20d4217",
+  measurementId: "G-5EE34K29KR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
+
 function sendEmail(name, email, message) {
     email.send({
         Host: "smtp.gmail.com",
