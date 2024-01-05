4cdb8ce4e25e90edffc99d876f574443a7b49761
Beomsu Kim <bek001@ucsd.edu>
2024-01-05 12:00:01 -0800

    referral form

--- scripts/inbox.js
+++ scripts/inbox.js
@@ -25,6 +25,14 @@ const db  = getFirestore(app);
 //     display_items(load_items(0));
 // }
 
+function appendLi(parent, itemData, check, text) {
+    const liClone = document.querySelector('#list-element').content.cloneNode(true);
+    if ( itemData[check] ) {
+        liClone.querySelector('li').textContent = text;
+        parent.appendChild(liClone);
+    }
+}
+
 function displayItem(item) {
     const itemData = item.data()
     const appointmentInboxE = document.querySelector('#inboxes-appointment');
@@ -37,38 +45,42 @@ function displayItem(item) {
             console.log(template);
             const clone = template.content.cloneNode(true);
             clone.querySelector('.update-time').textContent = itemData["updated-time"];
+            clone.querySelector('.patient-name').textContent = itemData['patient-fname'] + ' ' + itemData['patient-lname'];
+            clone.querySelector('.location').textContent = itemData['location'];
+
             clone.querySelector('.doctor-email').textContent = itemData['doctor-email'];
             clone.querySelector('.doctor-facility').textContent = itemData['doctor-facility'];
             clone.querySelector('.doctor-name').textContent = itemData['doctor-name'];
             clone.querySelector('.doctor-tel').textContent = itemData['doctor-tel'];
-            clone.querySelector('.lan-eng').textContent = itemData['lan-eng'];
-            clone.querySelector('.lan-kor').textContent = itemData['lan-kor'];
-            clone.querySelector('.lan-oth').textContent = itemData['lan-oth'];
-            clone.querySelector('.location').textContent = itemData['location'];
+
             
             clone.querySelector('.patient-dob').textContent = itemData['patient-dob'];
+            clone.querySelector('.patient-tel').textContent = itemData['patient-tel'];
             clone.querySelector('.patient-email').textContent = itemData['patient-email'];
-            clone.querySelector('.patient-dob').textContent = itemData['patient-dob'];
-            clone.querySelector('.patient-email').textContent = itemData['patient-email'];
-            clone.querySelector('.patient-name').textContent = itemData['patient-fname'] + ' ' + itemData['patient-lname'];
             clone.querySelector('.patient-gender').textContent = itemData['patient-male'];
             clone.querySelector('.patient-story').textContent = itemData['patient-story'];
-            clone.querySelector('.patient-tel').textContent = itemData['patient-tel'];
 
-            clone.querySelector('.reasons').textContent = 'line 58';
-            // clone.querySelector('reason-alzheimer').textContent = itemData['reason-alzheimer'];
-            // clone.querySelector('reason-headache').textContent = itemData['reason-headache'];
-            // clone.querySelector('reason-insomnia').textContent = itemData['reason-insomnia'];
-            // clone.querySelector('reason-neck').textContent = itemData['reason-neck'];
+            const reasonsEl = clone.querySelector('.reasons');
+            appendLi(reasonsEl, itemData, 'reason-alzheimer', 'Alzheimer');
+            appendLi(reasonsEl, itemData, 'reason-headache', 'Headache');
+            appendLi(reasonsEl, itemData, 'reason-insomnia', 'Insomnia');
+            appendLi(reasonsEl, itemData, 'reason-neck', 'Neck');
 
-            clone.querySelector('.services-requested').textContent = 'line64';
-            // clone.querySelector('service-eeg').textContent = itemData['service-eeg'];
-            // clone.querySelector('service-hot').textContent = itemData['service-hot'];
-            // clone.querySelector('service-leqembi').textContent = itemData['service-leqembi'];
-            // clone.querySelector('service-other').textContent = itemData['service-other'];
-            // clone.querySelector('service-special').textContent = itemData['service-special'];
-            // clone.querySelector('service-tms').textContent = itemData['service-tms'];
+            const servicesEl = clone.querySelector('.services-requested');
+            
+            appendLi(servicesEl, itemData, 'service-eeg', 'EEG');
+            appendLi(servicesEl, itemData, 'service-hot', 'HBOT');
+            appendLi(servicesEl, itemData, 'service-leqembi', 'LEQEMBI');
+            appendLi(servicesEl, itemData, 'service-other', 'Other');            
+            appendLi(servicesEl, itemData, 'service-special', 'Special');
+            appendLi(servicesEl, itemData, 'service-tms', 'TMS');
 
+            const languagesEl = clone.querySelector('.patient-lans');
+            appendLi(languagesEl, itemData, 'lan-eng', 'English');
+            appendLi(languagesEl, itemData, 'lan-kor', 'Korean');
+            appendLi(languagesEl, itemData, 'lan-spa', 'Spanish');
+            appendLi(languagesEl, itemData, 'lan-oth', 'Other');
+            
             clone.querySelector('.submitter-name').textContent = itemData['submitter-name'];
             referralInboxE.appendChild(clone);        
         } else {
